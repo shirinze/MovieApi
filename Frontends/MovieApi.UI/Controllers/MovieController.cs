5cd86413133d34ffba780828bf563fcfa6afac70
shirinze <sherize1373@gmail.com>
2025-04-21 14:05:57 +0300

    Update Uilayout

---
+++ Frontends/MovieApi.UI/Controllers/MovieController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.UI.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult MovieList()
+        {
+            ViewBag.v1 = "Movie";
+            ViewBag.v2 = "Home";
+            ViewBag.v3 = "MovieList";
+
+            return View();
+        }
+    }
+}
